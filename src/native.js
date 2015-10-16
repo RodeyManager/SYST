bbdc2f7fa07826d4a05276d28e903304ae071965
Rodey Luo <rodeyluo@gmail.com>
2015-10-16 19:28:26 +0800

    更新模块

--- src/native.js
+++ src/native.js
@@ -10,14 +10,14 @@
 
     'use strict';
 
-    var ua = navigator.userAgent.toUpperCase();
-    // 当前环境是否为Android平台
-    var IS_ANDROID = ua.indexOf('ANDROID') !== -1,
-    // 当前环境是否为IOS平台
+    var ua = navigator.userAgent.toUpperCase(),
+        // 当前环境是否为Android平台
+        IS_ANDROID = ua.indexOf('ANDROID') !== -1,
+        // 当前环境是否为IOS平台
         IS_IOS = ua.indexOf('IPHONE OS') !== -1,
-    // 当前环境是否为本地Native环境
+        // 当前环境是否为本地Native环境
         IS_NATIVE = (IS_ANDROID || IS_IOS) ? true : false,
-    //判断是否为移动设备
+        //判断是否为移动设备
         IS_PHONE = ua.indexOf('MOBILE') !== -1,
         IS_PAD = ua.indexOf('IPAD') !== -1,
         IS_MOBILE = (IS_PHONE || IS_PAD) ? true : false;
@@ -40,8 +40,9 @@
         isPad       : IS_PAD,
         isMobile    : IS_MOBILE,
         callNative  : function(name){
-            //TODO...
+            //TODO......
         }
     };
+
 
 })(SYST);
